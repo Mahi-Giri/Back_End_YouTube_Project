5a777d1750748eacdd66530f147bf00349f22791
Mahi <maheshgiri9561@gmail.com>
2024-03-12 20:20:13 +0530

    utils code distribution

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
 
 dotenv.config({
-    path: "./.env",
+    path: "./env",
 });
 
-connectDB();
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 3000, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log(`Connecting failed with error ${err}`);
+    });
