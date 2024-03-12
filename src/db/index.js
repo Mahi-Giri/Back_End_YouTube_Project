62bda647c6e155cfd07f71189e7b00c34485255e
Mahi <maheshgiri9561@gmail.com>
2024-03-12 22:50:36 +0530

    error fixed

--- src/db/index.js
+++ src/db/index.js
@@ -1,9 +1,9 @@
-import mongoose from "mongoose";
+import { connect } from "mongoose";
 import { DB_NAME } from "../constant.js";
 
 const connectDB = async () => {
     try {
-        const connectionResponce = await mongoose.connect(
+        const connectionResponce = await connect(
             `${process.env.MONGO_URL}/${DB_NAME}`
         );
         console.log(
