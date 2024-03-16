d5657b1cb905ec32895507b3c6e2b4b508e524ba
Mahesh Giri <girimahesh.m@gmail.com>
2024-03-16 20:16:49 +0530

    add Health care router and controllers

--- src/app.js
+++ src/app.js
@@ -29,9 +29,26 @@ app.use(express.static("public"));
 app.use(cookieParser());
 
 import userRouter from "./routes/user.routes.js";
+import healthcheckRouter from "./routes/healthcare.routes.js"
+// import tweetRouter from "./routes/tweet.routes.js"
+// import subscriptionRouter from "./routes/subscription.routes.js"
+// import videoRouter from "./routes/video.routes.js"
+// import commentRouter from "./routes/comment.routes.js"
+// import likeRouter from "./routes/like.routes.js"
+// import playlistRouter from "./routes/playlist.routes.js"
+// import dashboardRouter from "./routes/dashboard.routes.js"
 
+// http://localhost:8000/api/v1/<Your_Router_Name>
 app.use("/api/v1/users", userRouter);
-// http://localhost:8000/api/v1/users
+app.use("/api/v1/healthCheck", healthcheckRouter)
+// app.use("/api/v1/tweets", tweetRouter)
+// app.use("/api/v1/subscriptions", subscriptionRouter)
+// app.use("/api/v1/videos", videoRouter)
+// app.use("/api/v1/comments", commentRouter)
+// app.use("/api/v1/likes", likeRouter)
+// app.use("/api/v1/playlist", playlistRouter)
+// app.use("/api/v1/dashboard", dashboardRouter)
+
 
 
 export { app };
